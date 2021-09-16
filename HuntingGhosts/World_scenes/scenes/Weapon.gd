7df20763922f81592a8027cd75d8543505dce123
Sam Larsen <samlar10@kingshigh.school.nz>
2021-09-17 10:31:37 +1200

    17/09
    may loose everything file system seems to be fixed

--- HuntingGhosts/World_scenes/scenes/Weapon.gd
+++ HuntingGhosts/World_scenes/scenes/Weapon.gd
@@ -1,12 +1,14 @@
 extends Node
 
-export var fire_rate = 0.5
+export var pfire_rate = 0.2
+export var sfire_rate = 0.5
 export var clip_size = 30
 export var reload_rate = 1
 onready var ammo_label = $"../UI/Label"
 onready var gunray = $"../GunRay"
 onready var particles = preload("res://World_scenes/scenes/Particles.tscn")
 
+var aiming = false
 var current_ammo = clip_size
 var can_fire = true
 var reloading = false
@@ -19,7 +21,7 @@ func _process(delta):
 			print("bang")
 			can_fire = false
 			current_ammo -= 1
-			yield(get_tree().create_timer(fire_rate), "timeout")
+			yield(get_tree().create_timer(pfire_rate), "timeout")
 			var p = particles.instance()
 			gunray.get_collider().add_child(p)
 			p.global_transform.origin = gunray.get_collision_point()
@@ -31,3 +33,24 @@ func _process(delta):
 			current_ammo = clip_size
 			reloading = false
 			print("reloded")
+
+#add function where the  player "aims in" send signal out so that fire rate changes see trello for brainstorm
+#
+#	if Input.is_action_pressed("ui_aim") and can_fire and not aiming:
+#		if current_ammo > 0 and not reloading:
+#			aiming = true
+#			print("aimed")
+#			can_fire = false
+#			current_ammo -= 1
+#			yield(get_tree().create_timer(sfire_rate), "timeout")
+#			var p = particles.instance()
+#			gunray.get_collider().add_child(p)
+#			p.global_transform.origin = gunray.get_collision_point()
+#			can_fire = true
+#		elif not reloading:
+#			print("reloading")
+#			reloading = true
+#			yield(get_tree().create_timer(reload_rate), "timeout")
+#			current_ammo = clip_size
+#			reloading = false
+#			print("reloded")
